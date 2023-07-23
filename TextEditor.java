71bc9161b9758c711b9a72312d3680e5cc3c7569
Gustavo7327 <gustavosilva712493@gmail.com>
2023-07-23 17:11:55 -0300

    save file and open file

--- TextEditor.java
+++ TextEditor.java
@@ -1,7 +1,13 @@
 import java.awt.*;
 import java.awt.event.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 import javax.swing.*;
 import javax.swing.event.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 
 public class TextEditor extends JFrame implements ActionListener {
@@ -91,7 +97,52 @@ public void actionPerformed(ActionEvent e) {
 		Color color = colorChooser.showDialog(null, "Chooser a color", Color.black);
 		textArea.setForeground(color);
 	}
+
+	if(e.getSource()==openFile){
+		JFileChooser fileChooser = new JFileChooser();
+		fileChooser.setCurrentDirectory(new File("."));
+		FileNameExtensionFilter filter = new FileNameExtensionFilter("Text files", "txt");
+		fileChooser.setFileFilter(filter);
+		int response = fileChooser.showOpenDialog(null);
+		if(response == JFileChooser.APPROVE_OPTION){
+			File file = new File(fileChooser.getSelectedFile().getAbsolutePath());
+			Scanner fileIn = null;
+			try {
+				fileIn = new Scanner(file);
+				if(file.isFile()){
+					while(fileIn.hasNextLine()){
+						String line = fileIn.nextLine()+"\n";
+						textArea.append(line);
+					}
+				}
+			} catch (FileNotFoundException e1) {
+				e1.printStackTrace();
+			} finally{
+				fileIn.close();
+			}
+			
+		}
+	}
 	
+	if(e.getSource()==saveFile){
+		JFileChooser fileChooser = new JFileChooser();
+		fileChooser.setCurrentDirectory(new File("."));
+		int response = fileChooser.showOpenDialog(null);
+		if(response == JFileChooser.APPROVE_OPTION){
+			File file;
+			PrintWriter fileOut = null;
+			file = new File(fileChooser.getSelectedFile().getAbsolutePath());
+			try {
+				fileOut = new PrintWriter(file);
+				fileOut.println(textArea.getText());
+			} catch (FileNotFoundException e1) {
+				e1.printStackTrace();
+			} finally{
+				fileOut.close();
+			}
+		}
+	}
+
 	if(e.getSource()==exit) {
 		System.exit(0);
 	}
